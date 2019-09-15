1f8046d936b9d2b4809e203022f31952f68eb29c
Cristina Tarquini <tarquini@meso.net>
2019-09-15 18:12:05 +0200

    New cam path with shorter intro, hints in the middle, timing readjusted, animal card higher and larger, no shark &clowfish

--- AcidicOcean.js
+++ AcidicOcean.js
@@ -506,8 +506,8 @@ function GetActorIntersections(CameraScreenUv)
 
 
 
-const TimelineMinYear = 1790;
-const TimelineBigBangYear = 1813;
+const TimelineMinYear = 1800;
+const TimelineBigBangYear = 1823;
 const TimelineMinInteractiveYear = 1860;
 const TimelineMaxYear = 2160;
 const TimelineMaxInteractiveYear = 2100;
