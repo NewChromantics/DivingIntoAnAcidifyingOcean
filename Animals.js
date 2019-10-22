851c99181ef01b17646a518455178ac8eafacaa5
Graham Reeves <graham@grahamreeves.com>
2019-10-22 19:36:09 +0100

    Added random point order for water grid, with random offset per point

--- Animals.js
+++ Animals.js
@@ -203,15 +203,33 @@ function GenerateGridVertexes(Contents,OnVertex,OnMeta)
 {
 	const Width = 300;
 	const Height = 300;
-	for ( let x=0;	x<Width;	x++ )
+	
+	//	too slow to sort afterwards, make a list of indexes in a random order
+	//	generate indexes, then randomise. Fastest!
+	let Coords = Array.from( {length:(Width*Height)}, (_,i) => (i+1) );
+	Pop.Array.Shuffle(Coords);
+
+	function PushXY(x,y)
 	{
-		for ( let y=0;	y<Height;	y++ )
-		{
-			let u = Math.lerp( 0.2, 0.8, x / Width );
-			let v = Math.lerp( 1.0, 0.4, y / Height );
-			OnVertex(u,0,v);
-		}
+		//	offset each point randomly
+		let OffsetMax = 0.4;
+		let s = Math.Lerp( -OffsetMax, OffsetMax, Math.random() );
+		let t = Math.Lerp( -OffsetMax, OffsetMax, Math.random() );
+		s *= 1 / Width;
+		t *= 1 / Height;
+		let u = Math.lerp( 0.2, 0.8, x / Width ) + s;
+		let v = Math.lerp( 1.0, 0.4, y / Height ) + t;
+		OnVertex( u,0,v );
 	}
+	
+	for ( let i=0;	i<Coords.length;	i++ )
+	{
+		const xy_index = Coords[i];
+		let x = xy_index % Width;
+		let y = xy_index / Width;
+		PushXY( x, y );
+	}
+
 }
 
 function GetWaterMeta()
