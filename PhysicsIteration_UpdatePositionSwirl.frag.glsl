abda96fea9b5c0c117263ef69404ad5b10a1ffe1
Graham Reeves <graham@grahamreeves.com>
2019-11-11 16:50:51 +0000

    trying to fix swirls

--- PhysicsIteration_UpdatePositionSwirl.frag.glsl
+++ PhysicsIteration_UpdatePositionSwirl.frag.glsl
@@ -22,6 +22,9 @@ uniform float SplineTimeRange;
 uniform float StringStrips;
 
 uniform bool FirstUpdate;
+const float2 PositionScalarMinMax = float2(0.1,2.0);
+const float2 VelocityScalarMinMax = float2(0.005,0.5);
+
 
 uniform float3 AvoidRayStart;
 uniform float3 AvoidRayDirection;
@@ -139,21 +142,54 @@ float3 GetSpringTargetPos(float2 uv)
 	return SplinePos;
 }
 
+float3 GetScaledInput(float2 uv,sampler2D Texture,float2 ScalarMinMax)
+{
+	if ( FirstUpdate )
+		return float3(0,0,0);
+	
+	vec4 Pos = texture2D( Texture, uv );
+	Pos.xyz -= float3( 0.5, 0.5, 0.5 );
+	Pos.xyz *= 2.0;
+	Pos.xyz *= mix( ScalarMinMax.x, ScalarMinMax.y, Pos.w );
+	
+	return Pos.xyz;
+}
 
+
+float3 abs3(float3 xyz)
+{
+	return float3( abs(xyz.x), abs(xyz.y), abs(xyz.z) );
+}
+
+
+float4 GetScaledOutput(float3 Position,float2 ScalarMinMax)
+{
+	//	get the scalar, but remember, we are normalising to -0.5,,,0.5
+	//	so it needs to double
+	//	and then its still 0...1 so we need to multiply by an arbritry number I guess
+	//	or 1/scalar
+	float3 PosAbs = abs3(Position);
+	float Big = max( ScalarMinMax.x, max( PosAbs.x, max( PosAbs.y, PosAbs.z ) ) );
+	float Scalar = Range( ScalarMinMax.x, ScalarMinMax.y, Big );
+	Position /= Big;
+	Position /= 2.0;
+	Position += float3( 0.5, 0.5, 0.5 );
+	
+	return float4( Position, Scalar );
+}
 
 
 void main()
 {
 	//	gr: this should make sure it's sample middle of texel
-	vec4 Pos = texture2D( LastPositions, uv );
-	vec4 Vel = texture2D( Velocitys, uv );
+	vec3 Pos = GetScaledInput( uv, LastPositions, PositionScalarMinMax );
+	vec3 Vel = GetScaledInput( uv, Velocitys, VelocityScalarMinMax );
 	Pos += Vel * PhysicsStep;
-	Pos.w = 1.0;
 	
-	if ( FirstUpdate )
-		Pos.xyz = GetSpringTargetPos(uv);
+	//if ( FirstUpdate )
+		Pos = GetSpringTargetPos(uv);
 	
-	gl_FragColor = Pos;
+	gl_FragColor = GetScaledOutput( Pos, PositionScalarMinMax );
 }
 
 
