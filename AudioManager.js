7ec5974864281e2825065666ded5a42864bf6826
Graham Reeves <graham@grahamreeves.com>
2020-03-13 15:29:02 +0000

    Rewrote lowlevel audio player object to run async with an overriding state (from foreground/minimised/app state) so now audio pauses properly

--- AudioManager.js
+++ AudioManager.js
@@ -2,164 +2,36 @@
 //	this needs to evolve into the proper pop API
 Pop.Audio = {};
 
+//	replace this!
+const DomTriggerPromise = Pop.CreatePromise();
+
+function OnDomTrigger()
+{
+	DomTriggerPromise.Resolve();
+}
+window.addEventListener('click',OnDomTrigger,true);
+
+Pop.Audio.DefaultGetGlobalAudioState = function ()
+{
+	//	if non-null, this result overrides the state of any sound
+	const Foreground = Pop.WebApi.IsForeground();
+	const Minimised = Pop.WebApi.IsMinimised();
+
+	if (!Foreground || Minimised)
+		return 'Pause';
+
+	return null;
+}
 
 //	https://gist.github.com/wittnl/8a1a0168b94f3b6abfaa#gistcomment-1551393
 const SilentMp3Url = 'data:audio/mp3;base64,SUQzBAAAAAAAI1RTU0UAAAAPAAADTGF2ZjU2LjM2LjEwMAAAAAAAAAAAAAAA//OEAAAAAAAAAAAAAAAAAAAAAAAASW5mbwAAAA8AAAAEAAABIADAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMDV1dXV1dXV1dXV1dXV1dXV1dXV1dXV1dXV6urq6urq6urq6urq6urq6urq6urq6urq6v////////////////////////////////8AAAAATGF2YzU2LjQxAAAAAAAAAAAAAAAAJAAAAAAAAAAAASDs90hvAAAAAAAAAAAAAAAAAAAA//MUZAAAAAGkAAAAAAAAA0gAAAAATEFN//MUZAMAAAGkAAAAAAAAA0gAAAAARTMu//MUZAYAAAGkAAAAAAAAA0gAAAAAOTku//MUZAkAAAGkAAAAAAAAA0gAAAAANVVV';
 
-//	gr: on web (mobile explicitly) you cannot play() a Audio unless it's been triggered
-//		from a touch/interaction event.
-//		once you play() a Audio it is okay'd by the browser, AND you can change the src
-//		and play again
-//		SO, we keep a pool of allowed-sounds which we will swap around as requested
-//		this limits the number of concurrent sounds we can have, but that's probably fine
-class SoundPool
-{
-	constructor(PoolSize=8)
-	{
-		this.DisabledAudios = [];
-		this.UsedAudios = [];
-		this.FreeAudios = [];
-		for ( let i=0;	i<PoolSize;	i++ )
-			this.DisabledAudios.push(null);
-		
-		function OnClick()
-		{
-			Pop.Debug("ONCLICK", this);
-		}
-		
-		//	debug
-		//window.addEventListener('touchstart', OnClick.bind('window touchstart'), true );
-		//document.addEventListener('click', OnClick.bind('document click true'), true);
-		//document.addEventListener('click', OnClick.bind('document click false'), false);
-		//document.body.addEventListener('click', OnClick.bind('document.body click true'), true);
-		//document.body.addEventListener('mouseup', OnClick.bind('document.body mouseup true'), true);
-		
-		//	not a user event!
-		//window.addEventListener('touchstart', this.AllocAudios.bind(this), true );
-
-		if (Pop.GetPlatform() == "Web")
-		{
-			//	desktop & safari on ios
-			window.addEventListener('click',this.AllocAudios.bind(this),true);
-		}
-	}
-	
-	
-	OnSoundEnabled(Sound)
-	{
-		//	this sound is now usable
-		//	move to free
-		Pop.Debug("OnSoundEnabled",...arguments);
-		Pop.Array.MoveElementFromArrayToArray( Sound, this.DisabledAudios, this.FreeAudios );
-	}
-	
-	OnSoundError(Sound,Error)
-	{
-		//	this sound cannot be played
-		//	make sure this is in the disabled list
-		Pop.Debug("OnSoundError",Sound,Error);
-	}
-	
-	AllocAudios()
-	{
-		Pop.Debug("Alloc Audios");
-		const Pool = this;
-		function AllocSound(ExistingSound)
-		{
-			//	here we COULD check if a sound has not been enabled and then play() to allow it to be used
-			if ( ExistingSound != null )
-				return ExistingSound;
-			
-			//	do an intial play to enable
-			const Sound = new Audio();
-			Sound.src = SilentMp3Url;
-			function OnSoundPlay()
-			{
-				Pool.OnSoundEnabled( Sound );
-			}
-			function OnSoundError(Error)
-			{
-				Pool.OnSoundError( Sound, Error );
-			}
-			Sound.play().then( OnSoundPlay ).catch( OnSoundError );
-			
-			//	make a free function which puts the sound back in the pool
-			Sound.Free = function(){	Pool.Release(Sound);	};
-			return Sound;
-		}
-
-		this.DisabledAudios = this.DisabledAudios.map( AllocSound );
-		this.FreeAudios = this.FreeAudios.map( AllocSound );
-		this.UsedAudios = this.UsedAudios.map( AllocSound );
-
-		//window.removeEventListener('touchstart', this.AllocAudios );
-		//window.removeEventListener('click', this.AllocAudios );
-	}
-	
-	Pop()
-	{
-		Pop.Debug("Pop sound from pool");
-		if ( this.FreeAudios.length == 0 )
-			throw "No availible sounds in sound pool!";
-		
-		const Sound = this.FreeAudios.splice(0,1)[0];
-		this.UsedAudios.push( Sound );
-		return Sound;
-	}
-	
-	Release(Sound)
-	{
-		Pop.Debug("Release sound into pool");
-		
-		//	stop it playing
-		//	https://stackoverflow.com/a/28060352/355753
-		//	may need to check its loaded first...
-		Sound.pause();
-		//	"" triggers an error!
-		//Sound.src = "";
-		Sound.src = SilentMp3Url;
-		Sound.load();
-		
-		//	find in list
-		Pop.Array.MoveElementFromArrayToArray( Sound, this.UsedAudios, this.FreeAudios );
-	}
-}
-Pop.Audio.SoundPool = new SoundPool();
 Pop.Audio.SoundIdCounter = 1000;
 
 Pop.Audio.AllocSoundId = function()
 {
 	return Pop.Audio.SoundIdCounter++;
 }
-
-Pop.Audio.AllocSound = function()
-{
-	if ( Pop.GetExeArguments().includes('NoAudio') )
-	{
-		const Sound = new AudioFake();
-		return Sound;
-	}
-
-	try
-	{
-		//	pop a sound out of the pool
-		const Sound = Pop.Audio.SoundPool.Pop();
-		return Sound;
-	}
-	catch(e)
-	{
-		Pop.Debug("Alloc sound failed: "+ e);
-		const Sound = new AudioFake();
-		return Sound;
-	}
-}
-
-
-
-
-
-
-
 
 
 //	fake audio stub
@@ -178,160 +50,163 @@ let AudioFake = function()
 
 if ( Pop.GetPlatform() != 'Web' )
 {
+	//	audio is a web class. (is it window.Audio ?)
 	Audio = AudioFake;
 }
 
-//	make these params an object?
-//	note: this is a player, not an asset
+
+
 Pop.Audio.Sound = class
 {
-	constructor(Filename,Loop=false)
+	constructor(Url,Loop=false,GetOverrideState)
 	{
-		this.Loop = Loop;
-		this.Filename = Filename;
-		this.PlayPromise = null;	//	non-null if still waiting
-		this.AudioPlayer = null;
-		
-		this.Event_Error = null;
-		this.Event_Loaded = null;
-		this.Event_Ended = null;
-		this.Event_Suspend = null;
-		this.Event_Emptied = null;
-		this.Create();
-	}
-	
-	SetVolume(Volume)
-	{
-		//	this can be null if the sound has finished
-		if ( !this.AudioPlayer )
-			return;
-		this.AudioPlayer.volume = Volume;
-	}
-	
-	Create()
-	{
-		this.Event_Ended = function()
+		this.GetState = function ()
 		{
-			Pop.Debug("This sound has ended");
-			this.Destroy();
-		}.bind(this);
-		
-		this.Event_Suspended = function()
-		{
-			//	https://www.w3schools.com/jsref/event_onsuspend.asp
-			//	This can happen when the download has completed, or because it has been paused for some reason
-			Pop.Debug("This sound has been suspended");
-			//this.Destroy();
-		}.bind(this);
-		
-		this.Event_Emptied = function()
-		{
-			Pop.Debug("This sound has been empited");
-			//this.Destroy();
-		}.bind(this);
+			if (this.HasFinished())
+				return 'Stop';
 
-		//	callback when meta loaded, should use this for async init/load
-		this.Event_Loaded = function(Event)
-		{
-			Pop.Debug("Audio on loaded",Event,"Volume is " + this.AudioPlayer.volume, this.AudioPlayer );
-			//	gr: this will be initially paused if user has to interact with webpage first
-			if ( this.AudioPlayer.paused )
-				Pop.Debug("Audio has loaded, initially paused",this);
-		}.bind(this);
-		
-		this.Event_Error = function(Error)
-		{
-			if ( !this.AudioPlayer )
+			if (GetOverrideState)
 			{
-				Pop.Debug("On Error",Error,"with no AudioPlayer");
-				return;
+				const OverrideState = GetOverrideState();
+				if (OverrideState)
+					return OverrideState;
 			}
-			
-			Pop.Debug("On Error",Error,"WITH AudioPlayer");
-			const ErrorCode = this.AudioPlayer.error;
-			this.Destroy();
-			/*
-			1 = MEDIA_ERR_ABORTED - fetching process aborted by user
-			2 = MEDIA_ERR_NETWORK - error occurred when downloading
-			3 = MEDIA_ERR_DECODE - error occurred when decoding
-			4 = MEDIA_ERR_SRC_NOT_SUPPORTED - audio/video not supported
-			 */
+			return this.State;
 		}.bind(this);
 
-		//	need to handle when this fails
-		this.AudioPlayer = Pop.Audio.AllocSound();
-		this.AudioPlayer.loop = this.Loop;
-		this.AudioPlayer.autoplay = true;
-		
-		//	once would be good, but if it's not triggered it's left over, so we'll still manage it
-		//this.AudioPlayer.addEventListener('error', OnError, {once:true,passive:true} );
-		//this.AudioPlayer.addEventListener('loadeddata', OnLoaded, {once:true,passive:true}  );
-		//this.AudioPlayer.addEventListener('ended', OnEnded, {once:true,passive:true}  );
-		this.AudioPlayer.addEventListener('error',this.Event_Error);
-		this.AudioPlayer.addEventListener('loadeddata',this.Event_LoadedData);
-		this.AudioPlayer.addEventListener('ended',this.Event_Ended);
-		this.AudioPlayer.addEventListener('suspend',this.Event_Suspended);
-		this.AudioPlayer.addEventListener('emptied',this.Event_Emptied);
-
-		//	this triggers the load & play
-		this.AudioPlayer.src = this.Filename;
-		
-		const OnPlay = function(Args)
-		{
-			Pop.Debug("Play success",...arguments);
-		}.bind(this);
-		
-		const OnPlayError = function(Error)
-		{
-			Pop.Debug("Play error",Error,...arguments);
-		}.bind(this);
-		
-		this.AudioPlayer.load();
-		this.AudioPlayer.play().then(OnPlay).catch(OnPlayError);
+		this.Url = Url;
+		this.Loop = Loop;
+		this.StateQueue = new Pop.PromiseQueue();
+		this.State = 'Play';
+		this.Sound = new Audio();
+		this.Error = null;
+		this.Sound.src = Url;
+		this.AsyncUpdate().then(this.OnFinished.bind(this)).catch(this.OnError.bind(this));
 	}
-	
+
+	HasFinished()
+	{
+		//	we manually stopped
+		if (this.Unloaded === true)
+			return true;
+
+		return this.Sound.ended;
+	}
+
 	Destroy()
 	{
-		//	already dead
-		if ( !this.AudioPlayer )
-			return;
-
-		//	remove events
-		this.AudioPlayer.removeEventListener('error',this.Event_Error);
-		this.AudioPlayer.removeEventListener('loadeddata',this.Event_LoadedData);
-		this.AudioPlayer.removeEventListener('ended',this.Event_Ended);
-		this.AudioPlayer.removeEventListener('suspend',this.Event_Suspended);
-		this.AudioPlayer.removeEventListener('emptied',this.Event_Emptied);
-
-		const Audio = this.AudioPlayer;
-		this.AudioPlayer = null;
-		Audio.Free();
+		this.SetState('Stop');
 	}
-	
-	IsActive()
+
+	GetSoundState()
 	{
-		//	we auto delete the audio when it's finished,
-		//	so we can just check the player status
-		if ( !this.AudioPlayer )
-			return false;
-		return true;
+		if (!this.Sound)
+			return 'null';
+		let State = `RequestedState: ${this.State} `;
+		State += `paused: ${this.Sound.paused} `;
+		State += `SoundError: ${this.Sound.error} `;
+		State += `Error: ${this.Error} `;
+		return State;
 	}
-	
-	//	maybe call this enable?
-	Play(Play=true)
+
+	OnFinished()
 	{
-		Pop.Debug("Audio Play/Pause",Play);
-		if (!this.AudioPlayer)
-			return;
-		if ( Play )
-			this.AudioPlayer.play();
+		//Pop.Debug("AudioSound OnFinished");
+	}
+
+	OnError(Error)
+	{
+		this.Error = Error;
+		Pop.Debug(`Pop.Audio.Sound OnError ${Error}`);
+	}
+
+	async AsyncUpdate()
+	{
+		//	we could ditch this wait and call Play() first and IF there's an error, THEN wait for a click
+		await DomTriggerPromise;
+		//	load & prep to play
+		Pop.Debug("this.Sound.play",this.Sound);
+		await this.Sound.play();
+
+		this.Sound.loop = this.Loop;
+
+		//	immediately pause so we're in a ready state
+		this.Sound.pause();
+
+		//	set initial state
+		Pop.Debug(`InitialState = ${this.State}`);
+		this.SyncState();
+
+		//	wait for state to change
+		while (true)
+		{
+			const NewState = await this.StateQueue.Allocate();
+			//Pop.Debug(`NewState = ${NewState}`);
+			this.SyncState();
+		}
+	}
+
+	SetVolume(Volume)
+	{
+		this.Sound.volume = Volume;
+	}
+
+	SyncState()//private
+	{
+		const State = this.GetState();
+		//Pop.Debug(`Syncing state: ${State}`);
+
+		//	if not done after async waits, this might error
+		if (State == 'Play')
+		{
+			this.Sound.play();
+		}
+		else if (State == 'Pause')
+		{
+			this.Sound.pause();
+		}
+		else if (State == 'Stop')
+		{
+			//	there is no stop. Unload
+			//this.Sound.stop();
+			this.Sound.pause();
+			this.Sound.removeAttribute('src'); // empty source
+			this.Sound.load();
+			this.Unloaded = true;
+			if (!this.HasFinished())
+				throw "After stop, we should be finished";
+		}
 		else
-			this.AudioPlayer.pause();
+			throw `Unhandled state ${State}`;
+	}
+
+	SetState(NewState)
+	{
+		this.State = NewState;
+		this.OnStateChanged();
+	}
+
+	OnStateChanged()
+	{
+		const State = this.GetState();
+		this.StateQueue.Resolve(State);
+	}
+
+	SetStateStop()
+	{
+		//	stop it playing
+		//	https://stackoverflow.com/a/28060352/355753
+		//	may need to check its loaded first...
+		this.Sound.pause();
+		//	"" triggers an error!
+		//Sound.src = "";
+		this.Sound.src = SilentMp3Url;
+		this.Sound.load();
 	}
 }
 
 
-const TQueuedAudio = function(Filename,Loop,StartQuiet,GetVolume)
+const TQueuedAudio = function(Filename,Loop,StartQuiet,GetVolume,GetOverrideState)
 {
 	//	fades are 0..1. null if not yet invoked
 	this.Filename = Filename;
@@ -345,7 +220,7 @@ const TQueuedAudio = function(Filename,Loop,StartQuiet,GetVolume)
 		if ( !this.Audio )
 			return false;
 		//	audio has finished
-		if ( !this.Audio.IsActive() )
+		if ( this.Audio.HasFinished() )
 			return false;
 		return true;
 	}
@@ -398,16 +273,25 @@ const TQueuedAudio = function(Filename,Loop,StartQuiet,GetVolume)
 			this.Audio.SetVolume( Volume );
 		}
 	}
+
+	this.OnStateChanged = function ()
+	{
+		if (!this.Audio)
+			return;
+		this.Audio.OnStateChanged();
+	}
 	
 	//	init volume
 	if ( Filename !== null )
 	{
-		this.Audio = new Pop.Audio.Sound( Filename, Loop );
-		this.Audio.SetVolume( this.GetVolume() );
+		this.Audio = new Pop.Audio.Sound(Filename,Loop,GetOverrideState );
+		this.Audio.SetVolume(this.GetVolume());
+		this.Audio.SetState('Play');
 	}
+
 }
 
-const TAudioManager = function(GetCrossFadeDuration,GetMusicVolume,GetMusic2Volume,GetVoiceVolume,GetSoundVolume)
+const TAudioManager = function(GetCrossFadeDuration,GetMusicVolume,GetMusic2Volume,GetVoiceVolume,GetSoundVolume,GetOverrideState=Pop.Audio.DefaultGetGlobalAudioState)
 {
 	//	array of TQueuedAudio
 	//	the last element in the queue is NOT fading out, every other one is
@@ -415,7 +299,6 @@ const TAudioManager = function(GetCrossFadeDuration,GetMusicVolume,GetMusic2Volu
 	this.Music2Queue = [];
 	this.VoiceQueue = [];
 	this.Sounds = [];
-	this.Enabled = true;
 
 
 	this.ForegroundCheckLoop = async function ()
@@ -424,7 +307,10 @@ const TAudioManager = function(GetCrossFadeDuration,GetMusicVolume,GetMusic2Volu
 		{
 			const IsForeground = await Pop.WebApi.WaitForForegroundChange();
 			Pop.Debug("Audio manager foreground change",IsForeground);
-			this.Enable(IsForeground);
+			this.Sounds.forEach(s => s.OnStateChanged());
+			this.MusicQueue.forEach(s => s.OnStateChanged());
+			this.Music2Queue.forEach(s => s.OnStateChanged());
+			this.VoiceQueue.forEach(s => s.OnStateChanged());
 		}
 	}
 	this.ForegroundCheckLoop().then().catch(Pop.Debug);
@@ -460,22 +346,7 @@ const TAudioManager = function(GetCrossFadeDuration,GetMusicVolume,GetMusic2Volu
 		}
 		this.Sounds.forEach( UpdateSound );
 	}
-
-	this.Enable = function (NewEnabled)
-	{
-		//	change enabled state
-		if (this.Enabled == NewEnabled)
-			return;
-
-		//	super hacky
-		function EnableSound(Sound)
-		{
-			Sound.Play(NewEnabled);
-		}
-		this.Sounds.forEach(EnableSound);
-		this.Enabled = NewEnabled;
-	}
-
+	
 	this.Update = function(Timestep)
 	{
 		const FadeSecs = GetCrossFadeDuration();
@@ -502,7 +373,7 @@ const TAudioManager = function(GetCrossFadeDuration,GetMusicVolume,GetMusic2Volu
 		
 		let Loop = true;
 		let StartQuiet = false;
-		let NewSound = new TQueuedAudio( Filename, Loop, StartQuiet, GetMusicVolume );
+		let NewSound = new TQueuedAudio(Filename,Loop,StartQuiet,GetMusicVolume,GetOverrideState );
 		this.MusicQueue.push( NewSound );
 	}
 	
@@ -521,7 +392,7 @@ const TAudioManager = function(GetCrossFadeDuration,GetMusicVolume,GetMusic2Volu
 		
 		let Loop = true;
 		let StartQuiet = false;
-		let NewSound = new TQueuedAudio( Filename, Loop, StartQuiet, GetMusic2Volume );
+		let NewSound = new TQueuedAudio(Filename,Loop,StartQuiet,GetMusic2Volume,GetOverrideState );
 		this.Music2Queue.push( NewSound );
 	}
 
@@ -542,7 +413,7 @@ const TAudioManager = function(GetCrossFadeDuration,GetMusicVolume,GetMusic2Volu
 		
 		let Loop = false;
 		let StartQuiet = false;
-		let NewSound = new TQueuedAudio( Filename, Loop, StartQuiet, GetVoiceVolume );
+		let NewSound = new TQueuedAudio(Filename,Loop,StartQuiet,GetVoiceVolume,GetOverrideState );
 		this.VoiceQueue.push( NewSound );
 	}
 	
@@ -581,7 +452,8 @@ const TAudioManager = function(GetCrossFadeDuration,GetMusicVolume,GetMusic2Volu
 	
 	this.PlaySound = function(Filename)
 	{
-		const Sound = new Pop.Audio.Sound(Filename);
+		const Loop = false;
+		const Sound = new Pop.Audio.Sound(Filename,Loop,GetOverrideState);
 		Sound.Id = Pop.Audio.AllocSoundId();
 		this.Sounds.push( Sound );
 		Pop.Debug("PlaySound(",Filename);
---
+++ AudioTest.js
@@ -0,0 +1,128 @@
+Pop.Include = function (Filename)
+{
+	let Source = Pop.LoadFileAsString(Filename);
+	const Result = Pop.CompileAndRun(Source,Filename);
+}
+
+
+Pop.Include('AudioManager.js');
+Pop.Include('Hud.js');
+
+function HideUi()
+{
+	const Logo = new Pop.Hud.Label('Hud');
+	Logo.SetVisible(false);
+}
+HideUi();
+
+
+//	setup some test stuff for audio
+function ShowText(Text)
+{
+	const Element = document.getElementById('Ui');
+	Element.innerText = Text;
+}
+
+
+
+function RequestLoop()
+{
+	function It()
+	{
+		Iteration(...arguments);
+		RequestLoop();
+	}
+	window.requestAnimationFrame(It);
+}
+RequestLoop();
+
+
+
+
+class AudioMan
+{
+	constructor()
+	{
+		this.Sounds = [];
+		this.AsyncUpdate().then(Pop.Debug).catch(Pop.Debug);
+	}
+
+	async AsyncUpdate()
+	{
+		while (true)
+		{
+			await Pop.WebApi.WaitForForegroundChange();
+			this.Sounds.forEach(s => s.OnStateChanged());
+		}
+	}
+
+	GetDebugState()
+	{
+		let State = '';
+		function AppendSoundState(Sound)
+		{
+			State += Sound.Url + ': ';
+			State += Sound.GetSoundState();
+			State += '\n';
+		}
+		this.Sounds.forEach(AppendSoundState);
+		return State;
+	}
+
+	AllocSound(Url,Loop)
+	{
+		const NewSound = new AudioSound(Url,Loop);
+		this.Sounds.push(NewSound);
+		return NewSound;
+	}
+}
+
+
+function GetMusicVolume()
+{
+	return 1;
+}
+function GetSoundVolume()
+{
+	return 1;
+}
+function GetCrossFadeDuration()
+{
+	return 0.1;
+}
+
+const Aud2 = new TAudioManager(GetCrossFadeDuration,GetMusicVolume,GetMusicVolume,GetSoundVolume,GetSoundVolume);
+
+
+/*
+const AudioManager = new AudioMan();
+
+const Nightingales = AudioManager.AllocSound('Audio/Nightingales.mp3',false);
+const SonicRing = AudioManager.AllocSound('Audio/SonicRing.mp3',false);
+
+SonicRing.SetState('Play');
+setTimeout(function ()
+{
+	Nightingales.SetState('Play');
+},2000);
+*/
+function Iteration(Time)
+{
+	//let State = '\n\n\n\n\n' + AudioManager.GetDebugState();
+
+//	ShowText(State);
+
+	//Aud2.SetMusic('Audio/SonicRing.mp3');
+	Aud2.SetMusic('Audio/Nightingales.mp3');
+
+	Aud2.Update( 1/60 );
+}
+
+
+function OnClick()
+{
+	Aud2.PlaySound('Audio/SonicRing.mp3');
+}
+window.addEventListener('click',OnClick,true);
+
+
